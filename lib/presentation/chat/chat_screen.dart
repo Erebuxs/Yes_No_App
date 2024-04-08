028040f7ce77dfad5f1d50ec0bc39bd655a388f4
amores <carlos.amores19@outlook.com>
2024-04-07 23:29:36 -0600

    ListView - Area de los mensajes

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -15,6 +15,29 @@ class ChatScreen extends StatelessWidget {
         ),
         title: const Text('amores <3'),
       ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              })
+            ),
+            Text('mundo')
+          ],
+        ),
+      ),
     );
   }
 }
