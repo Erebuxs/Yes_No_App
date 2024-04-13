2511dadb681495d87b566d599249fb295a8e1b07
pc <carlos.amores19@outlook.com>
2024-04-13 13:29:49 -0600

    Mensajes de ella y FIN

--- lib/presentation/widget/chat/her_message_bubble.dart
+++ lib/presentation/widget/chat/her_message_bubble.dart
@@ -1,9 +1,15 @@
 // import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
 
 class HerMessageBubble extends StatelessWidget {
-  const HerMessageBubble({super.key});
+  final Message message;
+
+  const HerMessageBubble({
+    super.key, 
+    required this.message
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -17,14 +23,14 @@ class HerMessageBubble extends StatelessWidget {
             color: colors.secondary,
             borderRadius: BorderRadius.circular(20)
           ),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text('Hola Mundo', style: TextStyle(color: Colors.white),),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(message.text, style: const TextStyle(color: Colors.white),),
           ),
         ),
         const SizedBox(height: 5),
 
-        _ImageBubble(),
+        _ImageBubble( message.imageUrl!),
 
         const SizedBox(height: 10)
         //todo:imagen
@@ -34,6 +40,11 @@ class HerMessageBubble extends StatelessWidget {
 }
 
 class _ImageBubble extends StatelessWidget {
+
+  final String imageUrl;
+
+  const _ImageBubble(this.imageUrl);
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -41,7 +52,7 @@ class _ImageBubble extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Image.network(
-        "https://yesno.wtf/assets/no/31-cc391a4c0332a4ce5b4d4666f64a7b4a.gif",
+        imageUrl,
         width: size.width * 0.5,
         height: 125,
         fit: BoxFit.cover,
