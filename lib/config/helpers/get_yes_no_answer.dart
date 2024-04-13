9e87319363f1f88d0e18ea61eb28f2d3bcd71345
pc <carlos.amores19@outlook.com>
2024-04-13 12:37:43 -0600

    mappers

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -1,6 +1,7 @@
 
 import 'package:dio/dio.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/infrastructure/models/yes_no_model.dart';
 
 class GetYesNoAnswer {
   final _dio = Dio();
@@ -8,7 +9,13 @@ class GetYesNoAnswer {
   Future<Message> getAnswer() async {
     
     final response = await _dio.get('https://yesno.wtf/api');
-  
-    throw UnimplementedError();
+
+    final yesNoModel = YesNoModel.fromJsonMap( response.data );
+
+    return Message(
+      text: yesNoModel.answer, 
+      fromWho: FromWho.hers,
+      imageUrl: yesNoModel.image
+    );
   }
 }
--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -29,6 +29,11 @@ class ChatProvider extends ChangeNotifier {
 
   Future<void> herReply() async {
     final herMessage = await getYesNoAnswer.getAnswer();
+
+    messageList.add(herMessage);
+    notifyListeners();
+
+    moveScrollToBottom();
   }
 
   Future<void> moveScrollToBottom() async {
